944b88625d8c97572e8632503890c414819d50b8
Alakh <namdev.alakh@gmail.com>
2023-06-17 14:32:33 +0530

    added animation

--- script.js
+++ script.js
@@ -38,3 +38,18 @@ $( "#close").click(
 		$("#con24").hide();
 	}
 );
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+        else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
