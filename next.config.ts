3557472a6b4cf32dbb0f4a942f0c65c5145a24f7
alakhnamdev <namdev.alakh@gmail.com>
2025-07-16 13:13:46 +0530

    add workflow for deploying on github pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  devIndicators: false,
+  output: 'export',
+  images: { unoptimized: true }, // required for static export
+  trailingSlash: true, // optional, recommended
+  basePath: '/PortFolio', // replace with repo name for correct routing
+  assetPrefix: '/PortFolio',
 };
 
 export default nextConfig;
